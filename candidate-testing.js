e2bc31500019e924b94eefb106b7a4c67a793092
Tran Le <lilytranle@gmail.com>
2023-09-18 14:06:12 -0500

    part 1 of 3 first commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,31 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+candidateName = (" ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
+question = ("Who was the first American woman in space? ");
+// = [
+//    "1. Who was the first American woman in space? ",
+//    '2. True or false: 5 kilometer === 5000 meters?',
+//    "3. (5+3)/2 * 10 = ?",
+//    "4. Give the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+//    "5. What is the minimum crew size for the ISS?"
+//];
+
 let correctAnswer;
+correctAnswer = ("Sally Ride" );
+// = [
+//    "1. Sally Ride",
+//    "2. true",
+//    "3. 40",
+//    "4. Trajectory",
+//    "5. 3"
+//];
+
 let candidateAnswer;
+candidateAnswer =(" ");
+
 
 
 //TODO: Variables for Part 2
@@ -18,19 +39,33 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  let candidateName = input.askForName("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  let question = prompt(question);
+  candidateAnswer = input.answer("Enter your answer: ");
 }
-
+  //let askQuestion = [
+   // "1. Who was the first American woman in space? "
+    //"2. True or false: 5 kilometer === 5000 meters?",
+   // "3. (5+3)/2 * 10 = ?",
+   // "4. Give the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+   // "5. What is the minimum crew size for the ISS?"
+  //];
+  //for (let i = 0; i < askQuestion.length; i++) {
+   // console.log(askQuestion[i]);
+  //}
+//}
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if(candidateAnswers=="Sally Ride"){
+    alert("correct");  
+    return;
+  }
+    alert("incorrect");
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +77,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hi " + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }

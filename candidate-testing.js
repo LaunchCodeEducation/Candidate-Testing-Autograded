07ae9c02fbbb7921f9b9381acf5ec0187a9df317
RionHoward <148999262+RionHoward@users.noreply.github.com>
2023-10-30 22:48:06 -0400

    Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,12 +3,17 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = input.question("What is your name?");
+console.log(candidateName + " I hope you are doing well today!");;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-
+let question = ("Who was the first American woman in space? ");
+let correctAnswer = ("Sally Ride");
+let candidateAnswer = input.question("Who was the first American woman in space? ")
+if (candidateAnswer === correctAnswer){
+  console.log("Great job that is correct!");
+  } else {
+    console.log("Sorry that is incorrect!");
+  }
 
 //TODO: Variables for Part 2
 let questions;
@@ -16,9 +21,10 @@ let correctAnswers;
 let candidateAnswers;
 
 
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+function askForName(){
 
+  // TODO 1.1b: Ask for candidate's name //
+  
 }
 
 function askQuestion() {

193caa6607607e98872e971d32c4eb46cd69debb
tunisia <hjones0973@gmail.com>
2024-01-11 18:54:16 -0600

    Part 1 all 6 passing

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,28 +1,13 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-let questions = [
-  {question : "What is the best car to drive?"},
-  {question : "What is your birthdate?"},
-  {question : "Do you like snow?"},
-  {question : "What is today's date?"},
-  {question : "How many days are in a week?"}, 
-];
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
-
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let questions = [
-  {question: "Who was the first American woman in space? ", correctAnswer: "Sally Ride "},
-  {question: "What is your birthdate? ", correctAnswer: "9/13/1973 "},
-  {question: "Do you like snow? ", correctAnswer: "Yes/No"},
-  {question: "What is today's date? ", correctAnswer: "The day after yesterday. "},
-  {question: "How many days are in a week? ", correctAnswer: " 7 "}, 
-];
-let candidateAnswer = [];
-
-
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer ="";
 
 
 //TODO: Variables for Part 2
@@ -33,23 +18,27 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-var candidateName = input.question("What is your name?");
-candidateName = " ";
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = prompt(question);
+  question = "Who was the first American woman in space? ";
+  candidateAnswer = input.question(question);
+
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("you got it!");
-}else {
-  console.log("sorry, that's wrong. The correct answer is " + correctAnswer + "." );
-}
+  let question = "Who was the first American woman in space? ";
+  let correctAnswer = "Sally Ride";
+  candidateAnswer = "";
+  if (candidateAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+    console.log("Your answer is correct!");
+  } else {
+    console.log("Sorry, your answer is incorrect. The correct answer is " + correctAnswer + ".");
+  }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.

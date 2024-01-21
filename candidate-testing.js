a93bf2dbc1428591f26ebc03e9289f4fb7cf2dac
tunisia <hjones0973@gmail.com>
2024-01-21 15:41:25 -0600

    updated candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,10 +13,10 @@ let candidateAnswer ="";
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? " 
                 ,"True or false: 5 kilometer == 5000 meters? ",
-                 "(5 + 3)/2*10 = ? ", 
-                 "Given the array[8 , 'Orbit ', 'Trajectory ', 45  , what entry is at index 2? " ,
+                 ("(5 + 3)/2 * 10 = ? "), 
+                 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
                   "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride" ,"True", "40", "Trajectory", "3",];
+let correctAnswers = ["Sally Ride" ,"true", "40", "Trajectory", "3",];
 let candidateAnswers = [];
 let candidateName = "";
 

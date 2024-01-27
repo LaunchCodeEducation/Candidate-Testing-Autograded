de6522f8e0b6dc29ddb826cc41fa23dc462dc6d5
wisevaishu <vpm.win@gmail.com>
2024-01-27 15:27:05 -0600

    Part 3 - Candidate Testing - Done

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,7 +5,7 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question="Who was the first American woman in space?"
+let question="Who was the first American woman in space? "
 let correctAnswer='Sally Ride';
 let candidateAnswer="";
 
@@ -42,24 +42,27 @@ function gradeQuiz(candidateAnswers)
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or 
   //incorrectly  
   let grade=0;  //TODO 3.2 use this variable to calculate the candidates score.
+  let count =0;
+  console.log("************************************");
   for (i=0;i<correctAnswers.length;i++)
   {
-      if (candidateAnswers[i]===correctAnswers[i])
+      if (candidateAnswers[i].toLowerCase()===correctAnswers[i].toLowerCase())
       {
-        grade+=1;
-        console.log(`Question : ${questions[i]} 
-        Your Answer : ${correctAnswers[i]}
-        Correct Answer : ${candidateAnswers[i]}`);
+        count+=1;
+        console.log(`Question : ${questions[i]}
+Your Answer : ${correctAnswers[i]}
+Correct Answer : ${candidateAnswers[i]}`);
       }
       else
       {
-        //grade-=1;
         console.log(`Question : ${questions[i]} 
-        Your Answer : ${candidateAnswers[i]}
-        Correct Answevr : ${correctAnswers[i]}`);
+Your Answer : ${candidateAnswers[i]}
+Correct Answevr : ${correctAnswers[i]}`);
       }
   }
+  grade = (count/correctAnswers.length) * 100;
   console.log("Total Grade : " +grade);
+  
   return grade;
 }
 

ed984aa464e9803ddf967edfa3a8af3de49ea2de
Msinfiniti50 <hjones0973@gmail.com>
2024-01-29 19:17:10 -0600

    graded assignmen

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,64 +11,66 @@ let candidateAnswer ="";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? " 
-                ,"True or false: 5 kilometer == 5000 meters? ",
-                 ("(5 + 3)/2 * 10 = ? "), 
+let questions = ["Who was the first American woman in space? ", 
+                "True or false: 5 kilometer == 5000 meters? ",
+                 "(5 + 3)/2 * 10 = ? ", 
                  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
                   "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride" ,"true", "40", "Trajectory", "3",];
+let correctAnswers = ["Sally Ride" ,"true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let candidateName = "";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your name? ");
+candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  //questions = "Who was the first American woman in space? ";
-  //candidateAnswers = input.question(questions);
-  for (let i = 0; i < questions.length; i++) {    
-    candidateAnswers[i] = input.question(questions[i]);
-    if (candidateAnswers[i].toLowerCase().trim() === correctAnswers[i].toLowerCase().trim()) {
-      console.log("Your answer is correct!");
-    } else {
-      console.log("Sorry, your answer is incorrect. The correct answer is "  + `${correctAnswers[i]}` + ".");
-     }
-    }
-
-  return candidateAnswers;
+for (let i = 0; i < questions.length; i++) {  
+let currentAnswer = input.question(questions[i]);
+candidateAnswers.push(currentAnswer);
+ if (candidateAnswers[i].toLowerCase().trim() === correctAnswers[i].toLowerCase().trim()) {
+console.log("Your answer is correct!");
+} else {
+console.log("Sorry, your answer is incorrect. The correct answer is "  + `${correctAnswers[i]}` + ".");
+}  
+}
 }
 
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  //let question = "Who was the first American woman in space? ";
-  //let correctAnswer = "Sally Ride";
-  //candidateAnswer =[];
-  //for (let i = 0; i < candidateAnswers.length; i++) {
-  //if (candidateAnswers[i].toLowerCase().trim()
-  // === correctAnswers[i].toLowerCase().trim()) {
-  //  console.log("Your answer is correct!");
-  //} else {
-  // console.log("Sorry, your answer is incorrect. The correct answer is "  + `${correctAnswers[i]}` + ".");
-  // }
-  //}
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
- return grade;
+  let numberOfCorrectAnswers = 0;
+//console.log(`length of candidateAnswers.length)`);
+//console.log(`Length of correctAnswers: ${correctAnswers.length}`);
+  for (let i = 0; i < candidateAnswers.length; i++) { 
+//console.log(`candidateAnswers[${i}]: ${candidateAnswers[i]}`);//
+// console.log(`correctAnswers[${i}]: ${correctAnswers[i]}`);
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      numberOfCorrectAnswers++;
+   }
+  }
+  let grade = (numberOfCorrectAnswers / questions.length) * 100;
+  console.log(`Your score is ${grade}%`);
+  if (grade >= 70) {
+  console.log("Congratulations! You passed!"); 
+  } else {
+   console.log("Sorry, you did not pass.");  
+  }
+  return grade;
 }
+  
+   //TODO 3.2 use this variable to calculate the candidates score.
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
   console.log("Hello, " + candidateName + "!");
   askQuestion();
-  gradeQuiz(candidateAnswers);
+  gradeQuiz(candidateAnswers, correctAnswers);
 }
 
+//runProgram();
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
@@ -80,4 +82,5 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}
+
